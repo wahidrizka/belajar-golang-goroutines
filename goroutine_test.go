1ec69fe8f96074a7d3706786c9535694006f9634
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-09 18:14:45 +0700

    add tests for goroutines with DisplayNumber function

--- goroutine_test.go
+++ goroutine_test.go
@@ -16,3 +16,15 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutines(t *testing.T) {
+	for i := range 100000 {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
