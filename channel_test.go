2846f8abb23afaf3b2b5c9d2e9f3d3974259f12b
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-09 22:37:14 +0700

    add TestChannelAsParameter to test channel behavior with function parameter

--- channel_test.go
+++ channel_test.go
@@ -21,3 +21,21 @@ func TestCreateChannel(t *testing.T) {
 	time.Sleep(5 * time.Second)
 
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Wahid Rizka Fathurrohman"
+
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
