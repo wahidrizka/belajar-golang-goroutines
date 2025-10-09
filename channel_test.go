e8011f11ced285a161c43053b3d642db12031785
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-09 22:33:08 +0700

    add TestCreateChannel function to test channel behavior

---
+++ channel_test.go
@@ -0,0 +1,23 @@
+package belajar_golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Wahid Rizka Fathurrohman"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+
+}
