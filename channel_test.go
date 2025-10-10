6964e7b37bcbc57076f22c920b002907d47dc3f2
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-10 17:18:10 +0700

    add TestDefaultSelect to test select statement behavior with default case

--- channel_test.go
+++ channel_test.go
@@ -123,3 +123,31 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelect(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Menerima data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Menerima data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data...")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
