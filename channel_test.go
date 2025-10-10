348d8d7a16e3641730b7d56c902026cd2c807c4d
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-10 16:15:21 +0700

    add TestBufferedChannel to test buffered channel behavior

--- channel_test.go
+++ channel_test.go
@@ -59,3 +59,23 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Wahid"
+		channel <- "Rizka"
+		channel <- "Fathurrohman"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
