e53821466390a965b7946f9ba6a6df493fa12e21
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-13 13:26:56 +0700

    add: implement TestPool to demonstrate usage of sync.Pool with concurrent goroutines

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() any {
+			return "New"
+		},
+	}
+
+	pool.Put("Wahid")
+	pool.Put("Rizka")
+	pool.Put("Fathurrohman")
+
+	for range 10 {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
+	fmt.Println("Selesai")
+}
