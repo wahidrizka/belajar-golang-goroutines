8c4325a423d9f7cae4a34665d8296f7b9cc53601
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-10-13 13:13:30 +0700

    add: implement TestOnce to validate sync.Once functionality with concurrent goroutines

---
+++ once_test.go
@@ -0,0 +1,29 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+var counter = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	once := sync.Once{}
+	group := sync.WaitGroup{}
+
+	for range 100 {
+		go func() {
+			group.Add(1)
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	fmt.Println("Counter", counter)
+}
